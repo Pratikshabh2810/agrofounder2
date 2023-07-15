f94110d9be632dd7beab67d2cf8967f30395d8d2
Pratikshabh2810 <139431858+Pratikshabh2810@users.noreply.github.com>
2023-07-15 12:09:21 +0530

    Add files via upload

---
+++ agrofounder/script.js
@@ -0,0 +1,27 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let header = document.querySelector('.header-3');
+let scrollTop = document.querySelector('.scroll-top');
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+window.onscroll = () =>{
+
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 250){
+        header.classList.add('active');
+    }else{
+        header.classList.remove('active');
+    }
+
+    if(window.scrollY > 250){
+        scrollTop.style.display = 'initial';
+    }else{
+        scrollTop.style.display = 'none';
+    }
+
+}
